92a98736052b6e316f5a7ad68ff15a72382a8d39
Michael Miller <mmiller@esri.com>
2015-04-22 12:16:50 -0400

    Updated to the most current boilerplate.  demoMap.js json added

--- config/defaults.js
+++ config/defaults.js
@@ -19,7 +19,10 @@ define([], function () {
         "bingKey": "", //Enter the url to your organizations bing maps key if you want to use bing basemaps
         //Defaults to arcgis.com. Set this value to your portal or organization host name.
         "sharinghost": location.protocol + "//" + "www.arcgis.com",
-
+        "units": null,
+        //If your application needs to edit feature layer fields set this value to true. When false the map will
+        //be dreated with layers that are not set to editable which allows the FeatureLayer to load features optimally. 
+        "editable": false,
         "helperServices": {
             "geometry": {
                 "url": null
--- config/templateConfig.js
+++ config/templateConfig.js
@@ -22,31 +22,33 @@ define({
     "queryForGroupInfo": false,
     // When true, the template will query arcgis.com for the items contained within the group
     "queryForGroupItems": false,
+    // Use a local hosted webmap instead of a webmap on ArcGIS or portal.
+    "useLocalWebmap": false,
+    // Webmap file to use for the local webmap
+    "localWebmapFile": "config/demoMap",
     //When true the template will query arcgis.com for default settings for helper services, units etc. If you
     //want to use custom settings for units or any of the helper services set queryForOrg to false then enter
     //default values for any items you need using the helper services and units properties.
-    "queryForOrg": false,
+    "queryForOrg": true,
     //If you need localization set the localize value to true to get the localized strings
     //from the javascript/nls/resource files.
     //Note that we've included a placeholder nls folder and a resource file with one error string
     //to show how to setup the strings file.
     "queryForLocale": true,
-    // When true, this will query and mixin a common config file from the path specified in the index.html dojo packages
-    "queryForCommonConfig": false,
     // These are the options specified for querying items within the group. Modify these to get more items. You can also call the public template.queryGroupItems() method with these options as a parameter.
     "groupParams": {
-    	q: "group:\"${groupid}\" AND -type:\"Code Attachment\"",
-	    "sortField": "modified",
-	    "sortOrder": "desc",
-	    "num": 9,
-	    "start": 0
+        q: "group:\"${groupid}\" AND -type:\"Code Attachment\"",
+        "sortField": "modified",
+        "sortOrder": "desc",
+        "num": 9,
+        "start": 0
     },
     //This option demonstrates how to handle additional custom url parameters. For example
     //if you want users to be able to specify lat/lon coordinates that define the map's center or
     //specify an alternate basemap via a url parameter.
     "urlItems": [
-        "location",
-        "extent"// example param. ?theme=<my theme>
+      "location",
+      "extent"
     ],
     // Most users will not need to modify this value. For esri hosting environments only. Will automatically create a "sharinghost" and "proxyurl" for the application. Only set this is to true if the app is going to be stored on Esri's hosting servers. If you are using your own custom hosted portal, set the "sharinghost" in defaults.js instead of setting this to true.
     esriEnvironment: false
