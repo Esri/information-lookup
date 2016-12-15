301cd6c82eb60a206e929fe796bff3a575d14602
Michael Miller <mmiller@esri.com>
2016-12-14 20:36:52 -0500

    ArcGIS Online 4.4 version

---
+++ js/nls/id/resources.js
@@ -0,0 +1,22 @@
+define({
+  "map": {
+    "error": "Tidak dapat membuat peta",
+    "mouseToolTip": "Klik pada peta untuk memeriksa layanan"
+  },
+  "error": {
+    "layerNotFound": "Lapisan bukan berada di peta web",
+    "fieldNotFound": "Bidang tidak ditemukan",
+    "popupNotSet": "Popup tidak diaktifkan untuk lapisan ini",
+    "noLayersSet": "Tidak ada lapisan yang ditentukan pada pengaturan, kotak pencarian tidak akan berfungsi"
+  },
+  "page": {
+    "title": "Pencarian Informasi"
+  },
+  "ui": {
+    "basemapButton": "Peta Dasar"
+  },
+  "popup": {
+    "urlMoreInfo": "Info Lebih Lanjut",
+    "attachments": "Lampiran"
+  }
+});
